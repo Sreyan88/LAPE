38cae6e93f81354e7f67666a900627273bc264f4
cs20s030 <89025294+cs20s030@users.noreply.github.com>
2023-07-06 01:51:24 +0530

    Update __init__.py

--- src/encoder/__init__.py
+++ src/encoder/__init__.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 
 from src.encoder.audiontt import AudioNTT2020Task6
 from src.encoder.efficientnet import Efficient_Net
-from src.encoder.mast import MAST
+from src.encoder.mast import MAST as mast
 
-'''
+
 class MAST(nn.Module):
     """
     Encoder for our ICASSP 2023 paper:
@@ -13,7 +13,7 @@ class MAST(nn.Module):
     https://arxiv.org/pdf/2211.01515.pdf
     """
 
-    def __init__(self, label_dim=None, input_tdim=1024, imagenet_pretrain = False, audioset_pretrain = False, model_size='small_cls', return_cls=False, patch_drop=0.1):
+    def __init__(self, label_dim=None, input_tdim=1024, imagenet_pretrain = False, audioset_pretrain = False, model_size='base', return_cls=False, patch_drop=0.0):
         super(MAST, self).__init__()
 
         self.mast_model = mast(label_dim=label_dim, input_tdim=input_tdim, imagenet_pretrain = imagenet_pretrain, audioset_pretrain = audioset_pretrain, model_size=model_size, return_cls=return_cls)
@@ -24,4 +24,3 @@ class MAST(nn.Module):
         z = self.mast_model(batch, patch_drop=self.patch_drop)
 
         return z
-'''
